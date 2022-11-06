265b872960563fb9a19c1c68cf2c029a2fd6ec4a
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-11-06 16:07:47 +0100

    setting project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
 	content: [
-		"./app/**/*.{js,ts,jsx,tsx}",
 		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
+		"./app/components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		extend: {},
