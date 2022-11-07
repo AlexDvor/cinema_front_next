ad941a22d89889890565b42579cb3f7e39c70bd9
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-11-07 11:49:58 +0100

    add break points

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ module.exports = {
 		'./app/components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
+		screens: {
+			mobile: '320px',
+			// => @media (min-width: 640px) { ... }
+			tablet: '640px',
+			// => @media (min-width: 640px) { ... }
+			laptop: '1024px',
+			// => @media (min-width: 1024px) { ... }
+			desktop: '1280px',
+			// => @media (min-width: 1280px) { ... }
+		},
 		colors: {
 			primary,
 			black: colors.black,
