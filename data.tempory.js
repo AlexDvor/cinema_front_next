b79f5da1dbe079cf1900b117d77f6c2b47ac8384
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-11-07 14:20:01 +0100

    add temp data movies

---
+++ data.tempory.js
@@ -0,0 +1,15 @@
+export const movie = [
+	{
+		id: 1,
+		name: 'Alone in House',
+		url: './app/assets/images/templates/alone.jpg',
+	},
+	{ id: 2, name: '101', url: './app/assets/images/templates/101.jpg' },
+	{
+		id: 3,
+		name: 'Christmas',
+		url: './app/assets/images/templates/christmas.jpg',
+	},
+	{ id: 4, name: 'Grinch', url: './app/assets/images/templates/grinch.jpg' },
+	{ id: 5, name: 'Mouse', url: './app/assets/images/templates/mouse.jpg' },
+]
