db41991906a4e550b4fbcedf5a09c3b8e981af35
Oleksii Matviichuk <77227774+AlexDvor@users.noreply.github.com>
2022-11-14 11:06:48 +0100

    up api

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps, NextPage } from 'next'
 
 import Home from '@/components/screens/Home/Home'
 
+import { IActor } from '@/interfaces/actor.types'
 import { IHome } from '@/interfaces/page.types'
 
 import { ActorServices } from '@/services/actor.service'
@@ -17,9 +18,15 @@ const HomePage: NextPage<IHome> = (props) => {
 
 export const getStaticProps = async () => {
 	try {
-		const { results: actors } = await ActorServices.getPopularActors()
+		const { results: dataActors } = await ActorServices.getPopularActors()
 		const { results: movies } = await MovieService.getTrendingMovies()
 		// const slides = movies.slice(0, 4)
+
+		const actors: IActor[] = dataActors.slice(0, 7).map((actor: IActor) => ({
+			...actor,
+			title: actor.name,
+			poster_path: actor.profile_path,
+		}))
 
 		return {
 			props: { actors, movies },
